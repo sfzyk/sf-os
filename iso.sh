ba132a8f4eee16d8716a653858e0827ef16b6eff
sfzyk <767039957@qq.com>
2019-10-27 16:21:22 +0800

    a

--- iso.sh
+++ iso.sh
@@ -6,10 +6,10 @@ mkdir -p isodir
 mkdir -p isodir/boot
 mkdir -p isodir/boot/grub
 
-cp sysroot/boot/myos.kernel isodir/boot/myos.kernel
+cp sysroot/boot/sf-os.kernel isodir/boot/sf-os.kernel
 cat > isodir/boot/grub/grub.cfg << EOF
-menuentry "myos" {
-	multiboot /boot/myos.kernel
+menuentry "sf-os" {
+	multiboot /boot/sf-os.kernel
 }
 EOF
-grub-mkrescue -o myos.iso isodir
+grub-mkrescue -o sf-os.iso isodir
